dfa8c3c928961f0861e63040dae7148a99bad206
rio-design <rio.design.2399@gmail.com>
2026-04-30 17:01:51 +0900

    feat: ハンバーガーメニュー実装途中

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,18 @@
 // lucide Icons
 lucide.createIcons();
+
+// Hamburger menu
+const nav = document.getElementById("p-gnav");
+const humBtn = document.getElementById("ham-btn");
+
+humBtn.addEventListener('click', () => {
+    const isOpen = nav.classList.toggle('is-open');
+    humBtn.setAttribute('aria-expanded', isOpen);
+});
+
+document.addEventListener('click', e => {
+    if (!nav.contains(e.target) && !humBtn.contains(e.target)) {
+        nav.classList.remove('is-open');
+        humBtn.setAttribute('aria-expanded', false);
+    }
+});
