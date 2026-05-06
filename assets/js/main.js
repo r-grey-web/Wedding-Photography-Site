d7e59f7eef85b087940751b143063c30a2a2daab
rio-design <rio.design.2399@gmail.com>
2026-05-06 17:15:41 +0900

    feat: ハンバーガーメニュー完成

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,38 @@ lucide.createIcons();
 const nav = document.getElementById("p-gnav");
 const humBtn = document.getElementById("ham-btn");
 
+
 humBtn.addEventListener('click', () => {
-    const isOpen = nav.classList.toggle('is-open');
+    const isMobile = window.matchMedia("(max-width: 992px)").matches
+
+    if (isMobile) {
+        const isOpen = nav.classList.contains("is-open");
+        if (!isOpen) {
+            const navItems = document.querySelectorAll(".p-gnav .p-gnav__item");
+            const targetItems = Array.from(navItems).slice(0, 6);
+            targetItems.forEach((item, i) => {
+                item.style.transitionDelay = `${(i + 1) * 0.05}s`;
+            });
+        }
+    }
+
+    const opened = nav.classList.toggle('is-open');
+    document.body.classList.toggle('is-open', opened);
     humBtn.classList.toggle('is-open');
-    humBtn.setAttribute('aria-expanded', isOpen);
+    humBtn.setAttribute('aria-expanded', opened);
 });
 
-document.addEventListener('click', e => {
-    if (!nav.contains(e.target) && !humBtn.contains(e.target)) {
-        nav.classList.remove('is-open');
-        humBtn.setAttribute('aria-expanded', false);
-    }
-});
+function closeMenu() {
+    nav.classList.remove('is-open');
+    document.body.classList.remove('is-open');
+    humBtn.classList.remove('is-open');
+    humBtn.setAttribute('aria-expanded', false);
+}
+
+document.getElementById("overlay").addEventListener('click', closeMenu);
+
+
+
+
+
 
